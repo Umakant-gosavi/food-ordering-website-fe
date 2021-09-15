f09697dc1cf7a963d5697cd0f6362df4c6009708
Umakant-gosavi <83106105+Umakant-gosavi@users.noreply.github.com>
2021-09-15 15:51:48 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 //const DEVELOPMENT_URL = "http://localhost:6060";
-const PRODUCTION_URL = "https://powerful-shelf-75034.herokuapp.com";
+const PRODUCTION_URL = "https://thawing-bastion-44948.herokuapp.com";
 
 exports.API_URL = PRODUCTION_URL;
